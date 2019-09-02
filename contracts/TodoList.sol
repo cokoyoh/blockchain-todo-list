a0f8b2a7d8ab9259d0dc1cea6ead0efb7db0fcc5
cokoyoh <charlesokoyoh@gmail.com>
2019-09-02 13:19:49 +0300

    Create a smart contract and connect it to a local blockchain

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,5 +2,5 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     //this is a state variable any data written in the state variable is stored in the blockchain
-    uint taskCount = 0;
+    uint public taskCount = 0;
 }
