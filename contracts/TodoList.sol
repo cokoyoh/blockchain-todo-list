0c97c579eecfa6c38fcca622a9ef41269234b174
cokoyoh <charlesokoyoh@gmail.com>
2019-09-02 17:02:40 +0300

    List the todos from the blockchain onto the browser

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -4,10 +4,6 @@ contract TodoList {
     //this is a state variable any data written in the state variable is stored in the blockchain
     uint public taskCount = 0;
 
-    constructor() public {
-        createTask('Keep calm and learn smart contracts using Etherium');
-    }
-
     //model the data type
     struct Task {
         uint id;
@@ -17,6 +13,10 @@ contract TodoList {
 
     //this serves as the state variable/storage for the tasks we will create, more like the table tasks
     mapping (uint => Task) public tasks;
+
+    constructor() public {
+        createTask('Keep calm and learn smart contracts using Etherium');
+    }
 
     //create a method to add new tasks
     function createTask(string memory _content) public {
