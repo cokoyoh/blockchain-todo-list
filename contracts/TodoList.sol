383e99d4ecae7931e85aaedc5e5c8c056e89c60e
cokoyoh <charlesokoyoh@gmail.com>
2019-09-02 15:14:29 +0300

    Add the model for the TodoList and map it to store data in the blockchain

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,25 @@ pragma solidity ^0.5.0;
 contract TodoList {
     //this is a state variable any data written in the state variable is stored in the blockchain
     uint public taskCount = 0;
+
+    constructor() public {
+        createTask('Keep calm and learn smart contracts using Etherium');
+    }
+
+    //model the data type
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    //this serves as the state variable/storage for the tasks we will create, more like the table tasks
+    mapping (uint => Task) public tasks;
+
+    //create a method to add new tasks
+    function createTask(string memory _content) public {
+        taskCount++;
+
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
